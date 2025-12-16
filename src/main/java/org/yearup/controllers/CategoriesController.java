a71733c8679d4b8952f79b0dccbd2adc99107a49
kietnnn <kietnn@gmail.com>
2025-12-16 12:26:39 +0700

    4

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -6,9 +6,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.yearup.data.CategoryDao;
-import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
-import org.yearup.models.Product;
 
 import java.util.List;
 
@@ -18,62 +16,72 @@ import java.util.List;
 public class CategoriesController
 {
     private final CategoryDao categoryDao;
-    private final ProductDao productDao;
 
+    // Inject CategoryDao
     @Autowired
-    public CategoriesController(CategoryDao categoryDao, ProductDao productDao)
+    public CategoriesController(CategoryDao categoryDao)
     {
         this.categoryDao = categoryDao;
-        this.productDao = productDao;
     }
 
-    // GET /categories
+    // ---------------------------------------
+    // GET http://localhost:8080/categories
+    // ---------------------------------------
     @GetMapping
     public List<Category> getAll()
     {
         return categoryDao.getAllCategories();
     }
 
-    // GET /categories/{id}
+    // ---------------------------------------
+    // GET http://localhost:8080/categories/1
+    // ---------------------------------------
     @GetMapping("/{id}")
     public Category getById(@PathVariable int id)
     {
         Category category = categoryDao.getById(id);
+
         if (category == null)
         {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND);
         }
+
         return category;
     }
 
-    // GET /categories/{id}/products
-    @GetMapping("/{categoryId}/products")
-    public List<Product> getProductsById(@PathVariable int categoryId)
-    {
-        return productDao.listByCategoryId(categoryId);
-    }
-
-    // POST /categories (ADMIN only)
+    // ---------------------------------------
+    // POST http://localhost:8080/categories
+    // ADMIN only
+    // ---------------------------------------
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
-    public Category addCategory(@RequestBody Category category)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Category create(@RequestBody Category category)
     {
         return categoryDao.create(category);
     }
 
-    // PUT /categories/{id} (ADMIN only)
+    // ---------------------------------------
+    // PUT http://localhost:8080/categories/1
+    // ADMIN only
+    // ---------------------------------------
     @PutMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public void updateCategory(@PathVariable int id, @RequestBody Category category)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@PathVariable int id, @RequestBody Category category)
     {
         category.setCategoryId(id);
         categoryDao.update(id, category);
     }
 
-    // DELETE /categories/{id} (ADMIN only)
+    // ---------------------------------------
+    // DELETE http://localhost:8080/categories/1
+    // ADMIN only
+    // ---------------------------------------
     @DeleteMapping("/{id}")
     @PreAuthorize("hasRole('ADMIN')")
-    public void deleteCategory(@PathVariable int id)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable int id)
     {
         categoryDao.delete(id);
     }
